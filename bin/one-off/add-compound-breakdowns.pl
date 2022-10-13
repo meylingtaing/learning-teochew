70a8ce5ba9b0f0b0e7b94632603bc18bce08f424
Meyling <meylingtaing@gmail.com>
2022-10-13 17:10:08 -0400

    Add script to more easily add compound word components

---
+++ bin/one-off/add-compound-breakdowns.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/env -S perl -Ilocal/lib/perl5
+
+use strict;
+use warnings;
+
+use feature qw(say);
+
+binmode STDOUT, ':encoding(UTF-8)';
+
+use lib 'lib';
+use Teochew;
+use Teochew::Edit;
+
+use Teochew::Utils qw(); 
+use Input qw(confirm input_from_prompt);
+
+use Data::Dumper;
+use Term::ANSIColor qw(colored);
+
+my $db = Teochew::Edit->new;
+
+# Usage: add-compound-breakdowns.pl "hello"
+#
+# This just takes the english word as the first argument. No other args
+# are read
+my $input = shift @ARGV;
+
+die colored("Must provide an English word!", "red") . "\n"
+    unless defined $input;
+
+my %translation = $db->choose_translation_from_english($input);
+my $teochew = $translation{teochew};
+
+if (length($teochew->{chinese}) == 1) {
+    say "Only one Chinese character: $teochew->{chinese}!";
+    exit;
+}
+
+# XXX Remove this
+#warn Dumper(\%translation);
+
+# Break down the word character by character and see if there are other Teochew
+# entries that match
+my @chars     = split //,  $teochew->{chinese};
+my @syllables = split / /, $teochew->{pengim};
+
+my @child_teochew_ids;
+my $confirm_str = "Compound breakdown:";
+for (my $i = 0; $i < scalar @chars; $i++) {
+
+    # The pengim might have a tone change, but we only want the base tone for
+    # searching
+    $syllables[$i] =~ s/(\d)(\d)/$1/g;
+
+    my @rows = $db->dbh->selectall_array(qq{
+        select
+            Teochew.id teochew_id,
+            English.word english,
+            English.notes
+        from Teochew join English on Teochew.english_id = English.id
+        where chinese = ? and pengim = ?
+        order by Teochew.id
+    }, { Slice => {} }, $chars[$i], $syllables[$i]);
+
+    if (scalar @rows == 0) {
+        say colored("No translations found for $chars[$i] $syllables[$i]!",
+                    "red");
+        exit;
+    }
+
+    # If there's a single match, then use that. If there are multiple matches,
+    # prompt the user for the correct one
+    my $row_id = 0;
+    if (scalar @rows > 1) {
+        for (my $j = 0; $j < scalar @rows; $j++) {
+            my $msg = "$j: $rows[$j]{english}";
+            $msg .= " ($rows[$j]{notes})" if $rows[$j]{notes};
+            say $msg;
+        }
+        $row_id = input_from_prompt(
+            "Which translation to use for $chars[$i] $syllables[$i]?");
+    }
+
+    # XXX: I did not error check
+    push @child_teochew_ids, $rows[$row_id]{teochew_id};
+    $confirm_str .= "\n\t$rows[$row_id]{english}";
+}
+
+say $confirm_str;
