38bc5d39c4be6b8ce1fd704c1f96c296aa59405f
Kirsten Taing <meylingtaing@gmail.com>
2025-03-31 18:49:49 -0400

    Start writing a script for updating a Chinese row in the db
    It's not done yet

---
+++ bin/update-chinese.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/env -S perl -CA -Ilocal/lib/perl5
+
+# TODO: THIS SCRIPT IS NOT DONE
+
+use strict;
+use warnings;
+
+use feature qw(say);
+use lib 'lib';
+
+use utf8;
+binmode STDOUT, ':encoding(UTF-8)';
+binmode STDIN,  ':encoding(UTF-8)';
+
+use Data::Dumper;
+use Getopt::Long qw(GetOptionsFromArray);
+use Term::ANSIColor qw(colored);
+
+use Teochew;
+use Teochew::Edit;
+use Teochew::Utils qw(split_out_parens);
+use Input qw(confirm input_from_prompt);
+
+my $db = Teochew::Edit->new;
+
+# First argument should be the simplified Chinese character
+my $simplified = shift @ARGV;
+
+# Let's see what the user wants to update
+# TODO: Add more options
+my ($pengim);
+GetOptions(
+    "pengim=s" => \$pengim,
+);
+
+die colored ("Must provide pengim!", "red") . "\n"
+    unless $pengim;
+
+# Find the Chinese character in the database
+my $rows = Teochew::chinese_character_details($simplified);
+
+die colored("Could not find entry in the database for $simplified!", "red") . "\n"
+    unless $rows;
+
+my $row = $rows->[0];
+if (scalar @$rows > 1) {
+    # Need the user to select which Chinese entry to update
+    for (my $i = 0; $i < scalar @$rows; $i++) {
+        my $row = $rows->[$i];
+        say "$i: $row->{pengim}";
+    }
+    my $row_id = input_from_prompt(
+        "Which translation would you like to modify?");
+    $row = $rows->[$row_id];
+}
+
+$db->dbh->do("update Chinese set pengim = ? where id = ?",
+    undef, $pengim, $row->{chinese_id});
+
+# Check and see if there are any teochew entries to update
+my @teochew_rows = $db->dbh->selectall_array(qq{
+    select * from Teochew where Chinese like ? and pengim like ?
+}, { Slice => {} }, "%$simplified%", "%$row->{pengim}%");
+
+# Next, I need to double check that the pengim actually does match for the
+# particular syllable. And then I need to prompt the user to see if they want
+# to fix it
