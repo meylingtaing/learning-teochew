6ab0ecf830e4542fbd14814f101bbc2ff6a9a56a
Kirsten Taing <meylingtaing@gmail.com>
2025-04-10 18:25:02 -0400

    Be able to edit translation notes in insert-extra-notes.pl
    Not sure if this made more sense here or in the update-flashcard.pl
    script. Ideally, I would have put some of this logic in the
    Teochew::Edit class, but I was feeling kind of lazy

--- bin/insert-extra-notes.pl
+++ bin/insert-extra-notes.pl
@@ -8,22 +8,44 @@ use lib 'lib';
 
 binmode STDOUT, ':encoding(UTF-8)';
 
+use Data::Dumper;
 use Term::ANSIColor qw(colored);
+use Getopt::Long qw(GetOptionsFromArray);
 
 use Input qw(confirm input_via_editor);
 use Teochew;
 use Teochew::Edit;
 
-my ($english) = @ARGV;
+my ($english, @other_args) = @ARGV;
 
 die "Must include English!\n" unless $english;
+
+my $edit_translation_note;
+my $getopt_success = GetOptionsFromArray(\@other_args,
+    translation => \$edit_translation_note,
+);
+
+exit unless $getopt_success;
+
+my $db = Teochew::Edit->new;
 
 # Make sure the english word exists already
 my ($row) = Teochew::get_english_from_database(word => $english);
 die "$english does not exist!\n" unless $row;
 
+# Do we want to insert notes for the english word or for any translations?
+my $existing;
+my $translation_id;
+if ($edit_translation_note) {
+    my %translation = $db->choose_translation_from_english($english);
+    $translation_id = $translation{teochew}{translation_id};
+    $existing = Teochew::extra_translation_information_by_id($translation_id);
+}
+else {
+    $existing = Teochew::extra_information_by_id($row->{id});
+}
+
 # Check if we already have notes
-my $existing = Teochew::extra_information_by_id($row->{id});
 my $info = input_via_editor($existing);
 
 if ($info eq '') {
@@ -35,9 +57,26 @@ else {
 }
 
 if (confirm()) {
-    Teochew::Edit->insert_extra(
-        english_id => $row->{id},
-        info       => $info,
-    );
+    if ($edit_translation_note) {
+        if ($existing) {
+            $db->dbh->do(qq{
+                update TranslationExtra
+                set info = ?
+                where translation_id = ?
+            }, undef, $info, $translation_id);
+        }
+        else {
+            $db->dbh->do(qq{
+                insert into TranslationExtra (translation_id, info)
+                values (?, ?)
+            }, undef, $translation_id, $info);
+        }
+    }
+    else {
+        Teochew::Edit->insert_extra(
+            english_id => $row->{id},
+            info       => $info,
+        );
+    }
     say colored("Successfully updated the extra info for $english", 'green');
 }
