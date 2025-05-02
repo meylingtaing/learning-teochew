9b38ae6f73dac0011de309076718dcc7dbe627c3
Kirsten Taing <meylingtaing@gmail.com>
2025-05-02 17:09:43 -0400

    Add standalong script for adding a category

---
+++ bin/insert-category.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/env -S perl -CA -Ilocal/lib/perl5
+
+use strict;
+use warnings;
+
+use feature qw(say);
+use lib 'lib';
+
+use Data::Dumper;
+use Getopt::Long qw(GetOptionsFromArray);
+use Term::ANSIColor qw(colored);
+
+use Teochew;
+use Teochew::Edit;
+use Input qw(confirm input_from_prompt);
+
+# Usage:
+#   bin/insert-category.pl "NewCategory" "FlashcardSet"
+
+my ($category, $flashcardset) = @ARGV;
+
+my $db = Teochew::Edit->new;
+
+my %categories = map { $_->{name} => $_->{id} } Teochew::categories;
+my $category_id = $categories{$category};
+
+if ($category_id) {
+    say colored("Category $category already exists", "red");
+    exit;
+}
+
+die "Need a flashcard set!\n" unless $flashcardset;
+
+say "Creating new category $category under $flashcardset";
+if (confirm()) {
+    $category_id = $db->insert_category($category, $flashcardset);
+    say colored("Successfully added new category $category!", "green");
+}
+else {
+    exit;
+}
