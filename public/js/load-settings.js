c6ba4e16763dfd679bb51cb2dc7ef174b9b778b2
Kirsten Taing <meylingtaing@gmail.com>
2025-06-25 20:55:23 -0400

    Have settings dropdown control dark/light mode on the site
    And remove it from the link at the bottom of the page

--- public/js/load-settings.js
+++ public/js/load-settings.js
@@ -1,3 +1,4 @@
+// Stuff for dark mode
 function set_dark() {
     $('html').attr('data-bs-theme', 'dark');
     $('html').addClass('dark-mode');
@@ -13,9 +14,21 @@ if (localStorage.getItem('darkMode') == '1') {
 }
 
 $(function() {
-    $('#dark-mode-toggle').click(function(e) {
+
+    // Make sure the correct radio button (Dark mode/Light mode) is selected
+    // by default
+    if (localStorage.getItem('darkMode') == '1') {
+        $('#dark-mode-input').prop('checked', true);
+    }
+    else {
+        $('#light-mode-input').prop('checked', true);
+    }
+
+    // Toggle dark/light mode when the user changes the setting
+    $('input[name=display-mode]').on("change", function(e) {
         e.preventDefault();
-        if (localStorage.getItem('darkMode') == '1') {
+
+        if ($(this).val() == 'light-mode') {
             localStorage.setItem('darkMode', '0');
             set_light();
         }
